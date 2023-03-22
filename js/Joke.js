6d0b1e893b7010280b6e72dd30c8cef779a70546
Gauthier Lambeau <gauthier.lambeau@student.ehb.be>
2023-03-22 16:27:08 +0100

    niet klaar

---
+++ js/Joke.js
@@ -0,0 +1,22 @@
+"use strict";
+
+class Joke {
+    constructor(setup,delivery, category) {
+        this.setup = setup;
+        this.delivery = delivery;
+        this.category = category;      
+        }
+        
+    render() {
+        `<div>
+        <div class="item-field">Your field info</div>
+        <div class="item-field">Your field info</div>
+      </div>
+      
+      <div>
+        <img src="https://opendata.brussels.be/explore/dataset/street-art/files/9876c9a3f300f29c8ee619765c1ad768/300/" />
+      </div>`
+
+
+        }
+}
--- js/index.js
+++ js/index.js
@@ -1,15 +1,33 @@
 "use strict";
 
+import Joke from "./Joke.js";
+
 const app = {
     items: [],
     init() {
+        this.getData();
     },
     getData() {
+        fetch ("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,racist,sexist,explicit&amount=10")
+        .then(response => response.json())
+     .then (function(response){
+        return response.json();
+     })
+        .then (function(json){
+            json.jokes.forEach(function(jokeData){
+                const joke = new Joke(jokeData.category, jokeData.delivery, jokeData.setup);
+                app.items.push(joke);
+
+            });
+        });
     },
     onSearch() {
     },
-    render(message) {
+    render(list) {
+        document.querySelector("#list").innerHTML = list.map(item => item.render()).join(" ");
+        
     }
 
-}
-app.init()
+    
+};
+app.init();
